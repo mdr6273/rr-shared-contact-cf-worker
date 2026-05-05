8bd690926ae0f8fefa63d42e8f6e142f59772419
mdr6273 <60935091+mdr6273@users.noreply.github.com>
2026-05-04 22:33:05 -0700

    revert previous

--- worker.js
+++ worker.js
@@ -95,10 +95,10 @@
  *   3. Remove the route from their Cloudflare dashboard
  */
 const CUSTOMERS = {
-    "www.richardsonresources.com": {
+    "richardsonresources.com": {
         recipient: "mdr6273@gmail.com"
     },
-    "www.ceincsd.com": {
+    "ceincsd.com": {
         recipient: "customerservice@ceincsd.com"
     }
     // Add customers here following the pattern above
